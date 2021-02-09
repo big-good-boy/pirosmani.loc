cb27197600652c05e8a0c2d0137be461d5b8bd19
Andrey <drago999@bk.ru>
2021-02-09 12:06:49 +0500

    Раздел Все товары переделан под Bootstrap

--- js/main.js
+++ js/main.js
@@ -15,3 +15,23 @@ const menuSwiper = new Swiper('.swiper-container.menu-swiper-container', {
         prevEl: '.swiper-button-prev',
     }
 });
+
+// Stickjaw
+var SJ_setiings = {
+    options: {
+        hlw: true, // Height as width
+        wlh: true, // Width as height
+        hlt: true, // Width of the target element (target id is the same as for height)
+        wlt: true, // Height of the target element (target id is the same as for width)
+        alo: true, // All elements as one (height)
+    },
+    settings: {
+        windowResize: true, 
+        writeHystory: true 
+    }
+}
+var sj = new SJ({
+    settings: {
+        windowResize: true
+    }
+});
